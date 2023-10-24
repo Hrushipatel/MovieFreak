1ef856d5956a41fb730064d6916d2cf6d6d5f861
Hrushipatel <40917190+Hrushipatel@users.noreply.github.com>
2023-10-24 13:01:24 -0400

    Second

---
+++ src/main/java/dev/hrushi/movie_freak/MovieFreakApplication.java
@@ -0,0 +1,15 @@
+package dev.hrushi.movie_freak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MovieFreakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFreakApplication.class, args);
+    }
+    saddsa
+}
---
+++ src/test/java/dev/hrushi/movie_freak/MovieFreakApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hrushi.movie_freak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFreakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
